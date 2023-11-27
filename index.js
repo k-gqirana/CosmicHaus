6a3959e2049c6f8e07b7a361fcddce4dc811f9e9
k-gqirana <s216057361@mandela.ac.za>
2023-11-28 00:32:56 +0200

    Started Carousel Design

--- index.js
+++ index.js
@@ -21,3 +21,45 @@ $(document).ready(function () {
 });
 
 /* -------------------------------------------------------------------------- */
+
+// -------------------- Animation for Carousel -------------------- //
+
+const slides = document.querySelectorAll(".carousel-slide");
+const prevButton = document.querySelector(".fa-chevron-left");
+const nextButton = document.querySelector(".fa-chevron-right");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.style.display = i === index ? "block" : "none";
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto-scroll the carousel every 3 seconds
+const autoScrollInterval = setInterval(nextSlide, 3000);
+
+// Attach click event listeners to the previous and next buttons
+prevButton.addEventListener("click", () => {
+  clearInterval(autoScrollInterval); // Stop auto-scrolling
+  prevSlide();
+});
+
+nextButton.addEventListener("click", () => {
+  clearInterval(autoScrollInterval); // Stop auto-scrolling
+  nextSlide();
+});
+
+// Display the initial slide
+showSlide(currentSlide);
+
+// ---------------------------------------------------------------- //
